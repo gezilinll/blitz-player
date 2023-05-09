5fcd1c441f37262a06013dd646ce3e907857fc8d
zhangliang <zhangliang@gaoding.com>
2023-05-09 09:43:03 +0800

    feat: setup make package

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('index.html')
-  }
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-  app.whenReady().then(() => {
-    createWindow()
+  win.loadFile("index.html");
+};
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
-        }
-      })
-  })
+app.whenReady().then(() => {
+  createWindow();
 
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
     }
-  })
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
