c5545d18fb5c2252b970033aec54283cbed248b9
linbinghe <linbinghe@gmail.com>
2023-05-11 18:06:53 +0800

    feat: add button to play example

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
+<script lang="ts" setup>
+import PlayerView from '@/views/PlayerView.vue'; // @ is an alias to /src
+import { ref } from 'vue';
+
+const playing = ref(false);
+
+function clicked() {
+    playing.value = !playing.value;
+}
+</script>
+
 <template>
     <div style="width: 100vw; height: 70vh">
-        <PlayerView></PlayerView>
+        <PlayerView v-bind:playing="playing"></PlayerView>
     </div>
+    <button @click="clicked"></button>
 </template>
-
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import PlayerView from '@/views/PlayerView.vue'; // @ is an alias to /src
-
-@Options({
-    components: {
-        PlayerView,
-    },
-})
-export default class App extends Vue {}
-</script>
--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -2,14 +2,16 @@
     <canvas id="surface" style="width: 100%; height: 100%; background-color: chartreuse"></canvas>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import Player from '@/components/Player.vue'; // @ is an alias to /src
+<script lang="ts" setup>
+import { watch } from 'vue';
+import { ref } from 'vue';
 
-@Options({
-    components: {
-        Player,
-    },
-})
-export default class PlayerView extends Vue {}
+const props = defineProps<{
+    playing: boolean;
+}>();
+
+watch(
+    () => props.playing,
+    (value) => console.log('play status changed', value)
+);
 </script>
